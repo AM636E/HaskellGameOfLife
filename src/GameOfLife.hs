ac893dcc3b4a7327d5a60bc6b3a0a427b8ab0238
Nazarii Brenchuk <bnazariy@gmail.com>
2016-05-14 19:59:56 +0300

    Added effective rendering

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -8,10 +8,11 @@ module GameOfLife (
         runGameContiniously
     ) where
 
-import Data.List(intercalate)
+import Data.List(intercalate, elemIndex)
 import Data.List.Split(splitOn)
-import System.Console.ANSI(clearFromCursorToScreenBeginning, clearScreen)
+import System.Console.ANSI
 import Control.Concurrent(threadDelay)
+import Control.Monad(forM_)
 
 type Grid = [[Bool]]
 type Coordinate = (Int, Int)
@@ -96,11 +97,36 @@ glider w h = gameGrid (w, h) [(2,1), (1, 3), (2, 3), (3,3), (3,2)]
 runGame :: GameOptions -> IO ()
 runGame opts = putStr $ intercalate "\n" $ map showGrid (take (runs opts) $ iterate nextGeneration (grid opts))
 
+-- Redraws only lines with cells .
+showGridEffective :: Grid -> IO ()
+showGridEffective [] = return ()
+showGridEffective g = do
+    -- All indexes of lines with True ( cell ).
+    let lns = filter (\x -> x /= -1) $ map (\(i,_) ->
+                             case i of Just x -> x
+                                       Nothing -> -1)  (filter (\(_, x) -> (\t -> t) `any` x) $ map (\x -> (elemIndex x g, x)) g)
+
+    forM_ lns (
+        \x -> do
+        setCursorPosition x 0
+        clearLine
+        forM_ (render x) (\ch -> do
+                            if ch == '@'
+                                then do
+                                setSGR [SetColor Foreground Vivid Red]
+                                else return ()
+                            putChar ch
+                            setSGR [Reset]
+                            )
+        return ())
+    where
+        render x = [if x' then '@' else ' ' | x' <- (g!!x)]
+
 runGameContiniously :: GameOptions -> Int -> IO ()
 runGameContiniously opts delay = do
     let gen = (nextGeneration $ grid opts)
-    putStr $ showGrid gen
-    threadDelay ((1000 * 60) * delay)
+    showGridEffective gen
+    threadDelay ((100 * 60) * delay)
     clearFromCursorToScreenBeginning
     runGameContiniously (createOpts gen 10) delay
     return ()
